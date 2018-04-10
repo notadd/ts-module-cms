39972362ba47b417a5ea45117742f9b8049c277a
Eric <1945320167@qq.com>
2018-04-10 16:45:01 +0800

    Modify the code according to the tsLint standard.

--- src/cqrsCms/models/view/create-page.vm.ts
+++ src/cqrsCms/models/view/create-page.vm.ts
@@ -2,18 +2,18 @@ import { PageContentEntity } from "../../../entity/page.content.entity";
 import { PageEntity } from "../../../entity/page.entity";
 
 export class CreatePageVm {
-    //页面
+    /*页面*/
     public page?: PageEntity;
 
-    //内容
-    public content?: PageContentEntity[];
+    /*内容*/
+    public content?: Array<PageContentEntity>;
 
-    //每页条数
+    /*每页条数*/
     public limit: number;
 
-    //第几页
+    /*第几页*/
     public pages: number;
 
-    //数组
-    public array?: number[];
+    /*数组*/
+    public array?: Array<number>;
 }
