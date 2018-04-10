630d10825c049799b489c3a9cef9603669333580
Eric <1945320167@qq.com>
2018-04-10 16:51:47 +0800

    Modify the code according to the tsLint standard.

--- src/entity/page.content.entity.ts
+++ src/entity/page.content.entity.ts
@@ -9,33 +9,33 @@ import {
 } from "typeorm";
 import { PageEntity } from "./page.entity";
 
-@Entity('page_content_table')
+@Entity("page_content_table")
 export class PageContentEntity {
-    //内容Id
+    /*内容Id*/
     @PrimaryGeneratedColumn() id: number;
 
-    //页面Id
+    /*页面Id*/
     @Column() parentId: number;
 
-    //页面内容
+    /*页面内容*/
     @Column({ nullable: true, length: 10000 }) content: string;
 
     @ManyToOne(type => PageEntity, page => page.contents, {
         cascadeInsert: false,
         cascadeUpdate: false,
         cascadeRemove: false,
-        onDelete: 'CASCADE',
+        onDelete: "CASCADE",
         lazy: false,
         eager: false,
         nullable: true
     })
-    @JoinColumn({ name: 'parentId', referencedColumnName: 'id' })
+    @JoinColumn({ name: "parentId", referencedColumnName: "id" })
     page: PageEntity;
 
-    //创建时间
+    /*创建时间*/
     @CreateDateColumn() createAt: Date;
 
-    //修改时间
+    /*修改时间*/
     @UpdateDateColumn() updateAt: Date;
 
 }
