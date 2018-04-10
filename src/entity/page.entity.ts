d105ec26c912b0e3ff8e691dd42565bfe6e92fe8
Eric <1945320167@qq.com>
2018-04-10 16:52:47 +0800

    Modify the code according to the tsLint standard.

--- src/entity/page.entity.ts
+++ src/entity/page.entity.ts
@@ -2,38 +2,38 @@ import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Up
 import { PageContentEntity } from "./page.content.entity";
 import { PageClassifyEntity } from "./pageClassify.entity";
 
-@Entity('page_entity_table')
+@Entity("page_entity_table")
 export class PageEntity {
-    //页面id
+    /*页面id*/
     @PrimaryGeneratedColumn() id: number;
 
-    //页面标题
+    /*页面标题*/
     @Column({ length: 200 }) title: string;
 
-    //页面别名
+    /*页面别名*/
     @Column({ length: 200, nullable: true }) alias: string;
 
-    //页面分类Id
+    /*页面分类Id*/
     @Column({ nullable: true }) classifyId: number;
 
-    //页面分类Id
+    /*页面分类Id*/
     @Column({ nullable: true }) classify: string;
 
-    //创建时间
+    /*创建时间*/
     @CreateDateColumn() createAt: Date;
 
-    //修改时间
+    /*修改时间*/
     @UpdateDateColumn() updateAt: Date;
 
-    //无用
+    /*无用*/
     @Column({ default: false })
     check: boolean;
 
-    //页面内容
-    @OneToMany(type => PageContentEntity, PageContentEntity => PageContentEntity.page)
+    /*页面内容*/
+    @OneToMany(type => PageContentEntity, pageContentEntity => pageContentEntity.page)
     contents: PageContentEntity[];
 
-    @OneToMany(type => PageClassifyEntity, PageClassifyEntity => PageClassifyEntity.pages)
+    @OneToMany(type => PageClassifyEntity, pageClassifyEntity => pageClassifyEntity.pages)
     classifications: PageClassifyEntity[];
 
 }
--- src/entity/pageClassify.entity.ts
+++ src/entity/pageClassify.entity.ts
@@ -9,41 +9,41 @@ import {
 } from "typeorm";
 import { PageEntity } from "./page.entity";
 
-@Entity('page_classify_table')
+@Entity("page_classify_table")
 export class PageClassifyEntity {
-    //分类Id
+    /*分类Id*/
     @PrimaryGeneratedColumn() id: number;
 
-    //分类名称
+    /*分类名称*/
     @Column({ nullable: false, length: 120 }) title: string;
 
-    //分类别名
+    /*分类别名*/
     @Column({ nullable: false, length: 120 }) classifyAlias: string;
 
-    //内链
+    /*内链*/
     @Column({ nullable: true, length: 200 }) chainUrl: string;
 
-    //描述
+    /*描述*/
     @Column({ nullable: true, length: 200 }) describe: string;
 
-    //颜色
+    /*颜色*/
     @Column({ nullable: true, length: 40 }) color: string;
 
-    //父节点
+    /*父节点*/
     @Column({ nullable: true }) groupId: number;
 
-    @OneToMany(type => PageClassifyEntity, PageClassifyEntity => PageClassifyEntity.parent, { cascadeInsert: true })
-    children: PageClassifyEntity[];
+    @OneToMany(type => PageClassifyEntity, pageClassifyEntity => pageClassifyEntity.parent, { cascadeInsert: true })
+    children: Array<PageClassifyEntity>;
 
-    @ManyToOne(type => PageClassifyEntity, PageClassifyEntity => PageClassifyEntity.children, { cascadeInsert: true })
+    @ManyToOne(type => PageClassifyEntity, pageClassifyEntity => pageClassifyEntity.children, { cascadeInsert: true })
     parent: PageClassifyEntity;
 
-    //创建时间
+    /*创建时间*/
     @CreateDateColumn() createAt: Date;
 
-    //修改时间
+    /*修改时间*/
     @UpdateDateColumn() updateAt: Date;
 
-    @ManyToOne(type => PageEntity, PageEntity => PageEntity.classifications)
+    @ManyToOne(type => PageEntity, pageEntity => pageEntity.classifications)
     pages: PageEntity;
 }
