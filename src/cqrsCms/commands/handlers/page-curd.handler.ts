0661b5359d2a8b18e7d9f13c8a4c93c5e1959abe
Eric <1945320167@qq.com>
2018-04-10 16:32:04 +0800

    Modify the code according to the tsLint standard.

--- src/cqrsCms/commands/handlers/page-curd.handler.ts
+++ src/cqrsCms/commands/handlers/page-curd.handler.ts
@@ -11,13 +11,13 @@ export class CreatePageHandler implements ICommandHandler<PageParamCommand> {
     }
 
     async execute(command: PageParamCommand, resolver: (value?) => void): Promise<any> {
-        let id: string = '0';
+        const id = "0";
         let result;
         const page = this.publisher.mergeObjectContext(await this.repositoty.find(id));
         if (!command.pageEntity.array) {
             result = await this.pageService.curdCheck(command.pageEntity.page.alias, command.pageEntity.page.classifyId);
             if (result.Continue) {
-                page.createPage(command.pageEntity)
+                page.createPage(command.pageEntity);
             }
         } else {
             page.createPage(command.pageEntity);
