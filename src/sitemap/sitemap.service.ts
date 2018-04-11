a65be28969a7a912fe5819aa0eeff6b6e3db7d6a
Eric <1945320167@qq.com>
2018-04-11 10:51:33 +0800

    Modify the code according to the tsLint standard.

--- src/sitemap/sitemap.service.ts
+++ src/sitemap/sitemap.service.ts
@@ -4,6 +4,7 @@ import { Repository } from "typeorm";
 import { ArticleEntity } from "../entity/article.entity";
 import { PageEntity } from "../entity/page.entity";
 import { SitemapEntity } from "../entity/sitemap.entity";
+import builder from "xmlbuilder";
 
 @Component()
 export class SitemapService {
@@ -15,43 +16,43 @@ export class SitemapService {
 
     /**
      * 设置参数
-     * @param array_baidu_sitemap_options
+     * @param arrayOptions
      * @param {string} url
      * @returns {Promise<void>}
      */
-    public async commitXML(array_baidu_sitemap_options, url: string) {
+    public async commitXML(arrayOptions, url: string) {
         let sitemap: SitemapEntity = await this.siteRepository.findOneById(1);
-        if (sitemap == null) {
-            sitemap = array_baidu_sitemap_options;
+        if (sitemap === null) {
+            sitemap = arrayOptions;
             let fileName: string;
-            if (array_baidu_sitemap_options[ 'lc_XML_FileName' ]) {
-                fileName = 'sitemap_baidu';
+            if (arrayOptions.xmlFileName) {
+                fileName = "sitemap_baidu";
             } else {
-                fileName = 'sitemap';
+                fileName = "sitemap";
             }
-            sitemap.lc_XML_FileName = fileName;
+            sitemap.xmlFileName = fileName;
             await this.siteRepository.insert(sitemap);
         } else {
-            sitemap = array_baidu_sitemap_options;
+            sitemap = arrayOptions;
             let fileName: string;
-            if (array_baidu_sitemap_options[ 'lc_XML_FileName' ]) {
-                fileName = 'sitemap_baidu';
+            if (arrayOptions.xmlFileName) {
+                fileName = "sitemap_baidu";
             } else {
-                fileName = 'sitemap';
+                fileName = "sitemap";
             }
-            sitemap.lc_XML_FileName = fileName;
-            if (array_baidu_sitemap_options[ 'lc_is_Enabled_XML_Sitemap' ]) sitemap.lc_is_Enabled_XML_Sitemap = array_baidu_sitemap_options[ 'lc_is_Enabled_XML_Sitemap' ];
-            if (array_baidu_sitemap_options[ 'lc_page_select' ]) sitemap.lc_page_select = array_baidu_sitemap_options[ 'lc_page_select' ];
-            if (array_baidu_sitemap_options[ 'lc_post_select' ]) sitemap.lc_post_select = array_baidu_sitemap_options[ 'lc_post_select' ];
-            if (array_baidu_sitemap_options[ 'lc_is_update_sitemap_when_post' ]) sitemap.lc_is_update_sitemap_when_post = array_baidu_sitemap_options[ 'lc_is_update_sitemap_when_post' ];
-            if (array_baidu_sitemap_options[ 'lc_post_limit1000' ]) sitemap.lc_post_limit1000 = array_baidu_sitemap_options[ 'lc_post_limit1000' ];
+            sitemap.xmlFileName = fileName;
+            if (arrayOptions.xmlSiteMap) sitemap.xmlSiteMap = arrayOptions.xmlSiteMap;
+            if (arrayOptions.pageSelect) sitemap.pageSelect = arrayOptions.pageSelect;
+            if (arrayOptions.postSelect) sitemap.postSelect = arrayOptions.postSelect;
+            if (arrayOptions.updateWhenPost) sitemap.updateWhenPost = arrayOptions.updateWhenPost;
+            if (arrayOptions.postLimit1000) sitemap.postLimit1000 = arrayOptions.postLimit1000;
             await this.siteRepository.updateById(1, sitemap);
         }
     }
 
     public async UpdateXMLFile($mes = 0, url: string) {
-        let sitemap: SitemapEntity = await this.siteRepository.findOneById(1);
-        if (sitemap.lc_is_Enabled_XML_Sitemap) {
+        const sitemap: SitemapEntity = await this.siteRepository.findOneById(1);
+        if (sitemap.xmlSiteMap) {
             this.buildSitemapXml(url);
         }
     }
@@ -62,33 +63,30 @@ export class SitemapService {
      */
     public async getBaiduOptions(getBaiduOptions?) {
         getBaiduOptions = await this.siteRepository.findOneById(1);
-        let array_baidu_sitemap_options = new Array();
+        let arrayOptions = new SitemapEntity();
         if (getBaiduOptions) {
-            array_baidu_sitemap_options = getBaiduOptions;
+            arrayOptions = getBaiduOptions;
         } else {
-            if (!array_baidu_sitemap_options[ 'lc_XML_FileName' ]) {
-                array_baidu_sitemap_options[ 'lc_XML_FileName' ] = 'sitemap_baidu';
+            if (!arrayOptions) {
+                arrayOptions.xmlFileName = "sitemap_baidu";
             }
-            if (!array_baidu_sitemap_options[ 'lc_is_Enabled_XML_Sitemap' ]) {
-                array_baidu_sitemap_options[ 'lc_is_Enabled_XML_Sitemap' ] = '1';
+            if (!arrayOptions.xmlSiteMap) {
+                arrayOptions.xmlSiteMap = true;
             }
-            if (!array_baidu_sitemap_options[ 'lc_is_update_sitemap_when_post' ]) {
-                array_baidu_sitemap_options[ 'lc_is_update_sitemap_when_post' ] = '1';
+            if (!arrayOptions.updateWhenPost) {
+                arrayOptions.updateWhenPost = true;
             }
-            if (!array_baidu_sitemap_options[ 'lc_post_limit1000' ]) {
-                array_baidu_sitemap_options[ 'lc_post_limit1000' ] = '1';
+            if (!arrayOptions.postLimit1000) {
+                arrayOptions.postLimit1000 = false;
             }
-            if (!array_baidu_sitemap_options[ 'lc_post_select' ]) {
-                array_baidu_sitemap_options[ 'lc_post_select' ] = '1';
+            if (!arrayOptions.postSelect) {
+                arrayOptions.postSelect = true;
             }
-            if (!array_baidu_sitemap_options[ 'lc_page_select' ]) {
-                array_baidu_sitemap_options[ 'lc_page_select' ] = '1';
-            }
-            if (!array_baidu_sitemap_options[ 'lc_category_select' ]) {
-                array_baidu_sitemap_options[ 'lc_category_select' ] = '1';
+            if (!arrayOptions.pageSelect) {
+                arrayOptions.pageSelect = true;
             }
         }
-        return array_baidu_sitemap_options;
+        return arrayOptions;
     }
 
     /**
@@ -97,54 +95,53 @@ export class SitemapService {
      * @param $mes
      */
     public async buildSitemapXml(url: string) {
-        let array_baidu_sitemap_options = await this.getBaiduOptions().then(a => {
-            return a
+        const arrayOptions = await this.getBaiduOptions().then(a => {
+            return a;
         });
-        let lc_limit: number;
-        //只更新最近1000篇文章
-        if (array_baidu_sitemap_options[ 'lc_post_limit1000' ]) {
-            lc_limit = 1000;
+        let limit: number;
+        /*只更新最近1000篇文章*/
+        if (arrayOptions.postLimit1000) {
+            limit = 1000;
         } else {
-            lc_limit = 10000;
+            limit = 10000;
         }
-        let fs = require('fs');
-        let file = `${(__dirname).substring(0, (__dirname).lastIndexOf('/'))}/public/`;
-        let ws = fs.createWriteStream(`${file}${array_baidu_sitemap_options[ 'lc_XML_FileName' ]}.xml`);
-        let builder = require('xmlbuilder');
-        let root = builder.create('urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"');
-        let num: number = 1;
-        //链接包括文章
-        if (array_baidu_sitemap_options[ 'lc_post_select' ]) {
-            let mini: ArticleEntity[] = await this.artRepository.createQueryBuilder('art').orderBy('art.updateAt', 'DESC').limit(lc_limit / 2).getMany();
-            for (let t in mini) {
-                let newTime: Date = mini[ t ].updateAt;
-                let update: Date = new Date(newTime.getTime() + newTime.getTimezoneOffset() * 2 * 30 * 1000);
-                let item = root.ele('url');
-                let sequence = num++;
-                item.ele('sequence', sequence);
-                item.ele('loc', `${url.replace('"', "").replace('"', '')}/${mini[ t ].name}`);
-                //item.ele('loc','https://docs.nestjs.com/recipes/cqrs');
-                item.ele('changefreq', 'weekly');
-                item.ele('lastmod', `${update.toLocaleDateString()} ${update.toLocaleTimeString()}`);
+        const fs = require("fs");
+        const file = `${(__dirname).substring(0, (__dirname).lastIndexOf("/"))}/public/`;
+        const ws = fs.createWriteStream(`${file}${arrayOptions.xmlFileName}.xml`);
+        const root = builder.create("urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'");
+        let num = 1;
+        /*链接包括文章*/
+        if (arrayOptions.postSelect) {
+            const mini: Array<ArticleEntity> = await this.artRepository.createQueryBuilder("art").orderBy("art.updateAt", "DESC").limit(limit / 2).getMany();
+            for (const t in mini) {
+                const newTime: Date = mini[ t ].updateAt;
+                const update: Date = new Date(newTime.getTime() + newTime.getTimezoneOffset() * 2 * 30 * 1000);
+                const item = root.ele("url");
+                const sequence = num++;
+                item.ele("sequence", sequence);
+                item.ele("loc", `${url.replace("'", "").replace("'", "")}/${mini[ t ].name}`);
+                /*item.ele("loc","https:/*docs.nestjs.com/recipes/cqrs");*/
+                item.ele("changefreq", "weekly");
+                item.ele("lastmod", `${update.toLocaleDateString()} ${update.toLocaleTimeString()}`);
             }
         }
-        //链接包括页面
-        if (array_baidu_sitemap_options[ 'lc_page_select' ]) {
-            let mini: PageEntity[] = await this.pageRepository.createQueryBuilder('page').orderBy('page.updateAt', 'DESC').limit(lc_limit / 2).getMany();
-            for (let t in mini) {
-                let newTime: Date = mini[ t ].updateAt;
-                let update: Date = new Date(newTime.getTime() + newTime.getTimezoneOffset() * 2 * 30 * 1000);
-                let item = root.ele('url');
-                let sequence = num++;
-                item.ele('sequence', sequence);
-                item.ele('loc', `${url.replace('"', "").replace('"', '')}/${mini[ t ].title}`);
-                // item.ele('loc','https://docs.nestjs.com/recipes/cqrs');
-                item.ele('changefreq', 'weekly');
-                item.ele('lastmod', `${update.toLocaleDateString()} ${update.toLocaleTimeString()}`);
+        /*链接包括页面*/
+        if (arrayOptions.pageSelect) {
+            const mini: Array<PageEntity> = await this.pageRepository.createQueryBuilder("page").orderBy("page.updateAt", "DESC").limit(limit / 2).getMany();
+            for (const t in mini) {
+                const newTime: Date = mini[ t ].updateAt;
+                const update: Date = new Date(newTime.getTime() + newTime.getTimezoneOffset() * 2 * 30 * 1000);
+                const item = root.ele("url");
+                const sequence = num++;
+                item.ele("sequence", sequence);
+                item.ele("loc", `${url.replace("'", "").replace("'", "")}/${mini[ t ].title}`);
+                /* item.ele("loc","https:/*docs.nestjs.com/recipes/cqrs");*/
+                item.ele("changefreq", "weekly");
+                item.ele("lastmod", `${update.toLocaleDateString()} ${update.toLocaleTimeString()}`);
             }
         }
         root.end({ pretty: false });
-        ws.write(`<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="./sitemap.xsl"?>\n${root.toString().substring(0, root.toString().lastIndexOf('urlset') + 6)}>`);
+        ws.write(`<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="./sitemap.xsl"?>\n${root.toString().substring(0, root.toString().lastIndexOf("urlset") + 6)}>`);
         ws.end();
     }
 }
